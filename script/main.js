3b194bb8ee50ecd0af961bab243fdd2940a11cb9
rama1220 <muchammadramadhan12@gmail.com>
2023-09-26 19:32:45 +0700

    first commit

---
+++ script/main.js
@@ -0,0 +1,19 @@
+//Clear form before unload
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
+//Toggle & Responsive
+const navSlide = () => {
+
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+
+    });
+};
+navSlide();
